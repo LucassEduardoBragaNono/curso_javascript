b362729a7e7bc0a77f57d12ba009a63f42252306
LUCAS EDUARDO BRAGA NONO <0201392411018>
2024-09-26 09:16:39 -0300

    aula1

---
+++ aula1/codigos_aula/js/cond.js
@@ -0,0 +1,13 @@
+let nome = prompt("Say your name: ");
+
+let anoNasc = parseInt(prompt("Qual ano a desgraça começou: "));
+
+let idade = 2024 - anoNasc;
+
+if(idade > 17){
+    alert("vose e maio di idade parabuains");
+} else if(idade > 0){
+    alert("Menor de idade(pode cometer crimes si foda)");
+} else {
+    alert("A burrice do homem.");
+}
---
+++ aula1/codigos_aula/js/script.js
@@ -0,0 +1,42 @@
+// Exibe um alerta na tela
+alert("Olá Mundo!");
+
+/* 
+Aqui o comentário pode ter várias linhas
+Bom para quando precisamos adicionar muitos detalhes
+OU deixar marcações de versão e resposabilidade
+*/
+
+// Cria uma variavel com conteudo inicial
+var nome = "Essa é uma variavel de Texto (String)";
+var idade; // Cria uma variavel nula (null)
+idade = 36; // Adiciona um numero inteiro na variavel (number)
+var altura = 1.81; // Variavel com numero flutuante (number)
+var diretor = false; // Variavel do tipo booleana (boolean)
+var numeroTexto = "10";
+
+var nota1 = 10;
+var nota2 = 5;
+var media = (nota1 + nota2) / 2;
+
+var notas = [];
+notas[0] = 10;
+notas[1] = 5;
+
+var mediaArray = (notas[0] + notas[1]) / notas.length;
+
+if (numeroTexto === 10) {
+  alert("São Iguais");
+} else {
+  alert("Não são iguais");
+} 
+
+
+// otra fita
+
+let notas = [10, 4, 7, 6];
+
+let media = notas[0] + notas[1] + notas[2] + notas[3];
+let media2 = media / notas.length;
+console.log(media2);
+
